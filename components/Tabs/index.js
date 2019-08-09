436844d07e7a4e03e4c976c31aeeda419d95a897
Brandon-Harris <brandonharris177@gmail.com>
2019-08-09 10:29:04 -0600

    tabs done

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -13,21 +13,26 @@
 //         console.log(response)
 //     });
 
+const topics = document.querySelector('.topics');
+// console.log(topics);
+
 function getTopics() {
     axios.get('https://lambda-times-backend.herokuapp.com/topics')
     .then ((response) => {
-        console.log(response.data.topics)
-        return response.data.topics;
-    });
-}
+        const array = response.data.topics;
+        // console.log(array);
+        return array;
+    })
+    .then(function createTabs(array) {
+        array.forEach(topic =>{
+            const tab = document.createElement('div');
+            tab.classList.add('tab');
+            tab.textContent = topic;
+            // console.log(topic);
+            topics.appendChild(tab);
+
+        });
+    })
+};
 
 getTopics();
-
-function createTabs (array) {
-
-    array.forEach(topic =>{
-        const tab = document.createElement('div');
-        tab.classList.add('tab');
-        tab.textContent = topic;
-    });
-};
