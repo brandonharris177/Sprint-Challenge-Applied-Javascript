2a4046597c36d2c619f64bcc018a4ae4a659f0c7
Brandon-Harris <brandonharris177@gmail.com>
2019-08-09 10:40:26 -0600

    function to create cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,41 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+// axios.get('https://lambda-times-backend.herokuapp.com/articles')
+//     .then ((response) => {
+//         console.log(response)
+//     });
+
+function createCard () {
+        // define new elements
+        const card = document.createElement('div');
+        const headline = document.createElement('div');
+        const author = document.createElement('div');
+        const imgContainer = document.createElement('div');
+        const cardImg = document.createElement('img');
+        const authorsName = document.createElement('span');
+        
+        // Setup structure of elements
+        card.appendChild(headline);
+        card.appendChild(author);
+        author.appendChild(imgContainer);
+        imgContainer.appendChild(cardImg);
+        author.appendChild(authorsName);
+    
+        // set class names
+        card.classList.add('card');
+        headline.classList.add('headline');
+        author.classList.add('author');
+        imgContainer.classList.add('img-container');
+        
+        // set text content
+        headline.textContent = `test`;
+        // cardImg.src = ``;
+        authorsName.textContent = `test`;
+        
+        console.log(card);
+        return card;
+}
+
+createCard();
