4cd894697d8e692f819c2d05eb562072836ac5e0
Brandon-Harris <brandonharris177@gmail.com>
2019-08-09 10:01:51 -0600

    working on tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,27 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+// axios.get('https://lambda-times-backend.herokuapp.com/topics')
+//     .then ((response) => {
+//         console.log(response)
+//     });
+
+function getTopics() {
+    axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then ((response) => {
+        console.log(response.data.topics)
+        return response.data.topics;
+    });
+}
+
+getTopics();
+
+function createTabs (array) {
+
+    array.forEach(topic =>{
+        const tab = document.createElement('div');
+        tab.classList.add('tab');
+        tab.textContent = topic;
+    });
+};
