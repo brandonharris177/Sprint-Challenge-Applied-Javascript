6d26255308315f6f0fc4731963cc28cbd9c8194f
Brandon-Harris <brandonharris177@gmail.com>
2019-08-09 09:40:13 -0600

    header created in console

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,30 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header() {
+    // define new elements
+    const headerComponent = document.createElement('div');
+    const headerDate = document.createElement('span');
+    const headerName = document.createElement('h1');
+    const headerTemp = document.createElement('span');
+    
+    // Setup structure of elements
+    headerComponent.appendChild(headerDate);
+    headerComponent.appendChild(headerName);
+    headerComponent.appendChild(headerTemp);
+    
+    // set class names
+    headerComponent.classList.add('header');
+    headerDate.classList.add('date');
+    headerTemp.classList.add('temp');
+    
+    // set text content
+    headerDate.textContent = "SMARCH 28, 2019";
+    headerName.textContent = "Lambda Times";
+    headerTemp.textContent = "98°";
+    
+    console.log(headerComponent);
+    return headerComponent;
+}
+
+Header();
